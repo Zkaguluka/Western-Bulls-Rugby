4bb9752d6805eff49cedef3434ddce592367b656
zack <zckaguluka@gmail.com>
2016-09-03 09:43:15 +0300

    Added the links to the shop, among other chenges

---
+++ js/bullsJs.js
@@ -0,0 +1,15 @@
+$(
+
+	$("#homepageSubscribeButton").click(function() {
+		var userName, email;
+		userName= $("#homepageUserName").val();
+		email= $("#homepageUserEmail").val();
+		alert(userName);
+
+		if (userName.length==0 && email.length==0) {
+			 
+		}
+
+	});
+
+);
